e9e31f15631b908f0de26f610511d0f188c21d7d
harshbisht27 <harshbisht180@gmail.com>
2024-11-12 20:03:04 +0530

    changes in summary

--- src/dashboard/resume/components/preview/SummeryPreview.jsx
+++ src/dashboard/resume/components/preview/SummeryPreview.jsx
@@ -1,28 +1,11 @@
-import React from 'react';
+import React from 'react'
 
-function SummeryPreview({ resumeInfo }) {
+function SummeryPreview({resumeInfo}) {
   return (
-    <div>
-      <h2
-        className="font-bold text-xl text-center"
-        style={{
-          color: resumeInfo?.themeColor,
-        }}
-      >
-        Summary
-      </h2>
-      <hr
-        className="border-[1.5px] my-2"
-        style={{
-          borderColor: resumeInfo?.themeColor,
-        }}
-      />
-      <div className="my-4">
-        {/* Display the summery content */}
-        <p className="text-xs text-justify">{resumeInfo?.summery}</p>
-      </div>
-    </div>
-  );
+    <p className='text-xs'>
+        {resumeInfo?.summery}
+    </p>
+  )
 }
 
-export default SummeryPreview;
+export default SummeryPreview
